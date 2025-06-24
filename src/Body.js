3441d61291da608992d403c460373193df435d9b
Ana Clara <ana.devcodes@gmail.com>
2025-06-23 22:09:29 -0300

    Update Body.js

--- src/Body.js
+++ src/Body.js
@@ -6,7 +6,7 @@ import Stories from "./Stories";
 export default function Body() {
   return (
     <div className="corpo">
-      <div class="esquerda">
+      <div className="esquerda">
         <Stories />
         <Posts />
       </div>
